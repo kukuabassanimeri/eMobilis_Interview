896a8247046c231ac948e07dd2bb1a1652692570
Abass_Kuku <abasskuku319@gmail.com>
2026-02-14 13:52:17 +0300

    removed redundant create view

--- our_student/urls.py
+++ our_student/urls.py
@@ -6,9 +6,8 @@ from . import views
 urlpatterns = [
     
     # API Views
-    path('student-list/', views.StudentListApiView.as_view(), name='student-list'),
-    path('student/create/', views.StudentCreateAPIView.as_view(), name='student-create'),
-    path('student/<int:pk>/', views.StudentRetrieveUpdateDestroyAPIView.as_view(), name='stduent-rud'),
+    path('student-list/', views.StudentListCreateApiView.as_view(), name='student-list'),
+    path('student/<int:pk>/', views.StudentRetrieveUpdateDestroyAPIView.as_view(), name='student-rud'),
     
     # Render the frontend view
     path('students/', views.StudentsView, name='students')
--- our_student/views.py
+++ our_student/views.py
@@ -5,13 +5,8 @@ from rest_framework import generics
 from .models import StudentModel
 from .serializers import StudentSerializer
 
-# List Students from the database.
-class StudentListApiView(generics.ListAPIView):
-    queryset = StudentModel.objects.all()
-    serializer_class = StudentSerializer
-
-# Add  students to the database.
-class StudentCreateAPIView(generics.CreateAPIView):
+# List Students from the database as well as post student to the database.
+class StudentListCreateApiView(generics.ListCreateAPIView):
     queryset = StudentModel.objects.all()
     serializer_class = StudentSerializer
 
